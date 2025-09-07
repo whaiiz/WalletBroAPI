b98df32fd6d0a981b8f20d472f07e6f692c6cdc9
Jaime Reis <jaime_reis18@outlook.pt>
2025-09-07 19:52:24 +0000

    Creation of entites scripts and domain entities

---
+++ WalletBroAPI/WalletBro.Core/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿namespace WalletBro.Core.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    
+    public string FirstName { get; set; } = string.Empty;
+    
+    public string LastName { get; set; } = string.Empty;
+    
+    public string Email { get; set; } = string.Empty;
+    
+    public DateTime DateOfBirth { get; set; } = DateTime.MinValue!;
+    
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+}
---
+++ WalletBroAPI/WalletBro.Infrastructure/Scripts/03-ExpenseDetail.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE ExpenseDetail(
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    Name NVARCHAR(100),
+    UnitPrice MONEY,
+    UnitType NVARCHAR(50),
+    InvoiceId int not null,
+    CreatedAt DATETIME2 DEFAULT GETDATE(),
+    CONSTRAINT Fk_ExpenseDetail_Invoice
+        FOREIGN KEY (InvoiceId)
+        REFERENCES Invoice(Id)
+        ON DELETE CASCADE
+        ON UPDATE NO ACTION
+)
