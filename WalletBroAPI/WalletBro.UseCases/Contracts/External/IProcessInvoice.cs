f383e2de63a61a1a11cacb9fdf74aad81050ec41
Jaime Reis <jaime_reis18@outlook.pt>
2025-11-12 21:51:46 -0100

    Created contracts for infrastructure: IInvoiceRepository.cs and IProcessInvoice

---
+++ WalletBroAPI/WalletBro.UseCases/Contracts/External/IProcessInvoice.cs
@@ -0,0 +1,8 @@
+﻿using WalletBro.UseCases.Contracts.External.DTOs;
+
+namespace WalletBro.UseCases.Contracts.External;
+
+public interface IProcessInvoice
+{
+    Task<ProcessInvoiceResponse> ProcessInvoiceAsync(ProcessInvoiceRequest req);
+}
