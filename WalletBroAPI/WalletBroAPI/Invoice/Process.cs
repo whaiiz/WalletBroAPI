ba28128c215616cec36c407b1593acf494b4d190
Jaime Reis <jaime_reis18@outlook.pt>
2025-11-16 07:24:26 -0100

    Implemented process endpoint

--- WalletBroAPI/WalletBroAPI/Invoice/Process.cs
+++ WalletBroAPI/WalletBroAPI/Invoice/Process.cs
@@ -1,9 +1,11 @@
 ﻿using FastEndpoints;
+using MediatR;
 using Microsoft.AspNetCore.Http.HttpResults;
+using WalletBro.UseCases.Invoice.Process;
 
 namespace WalletBroAPI.Invoice;
 
-public class Process : Endpoint<ProcessInvoiceRequest,
+public class Process(IMediator mediator) : Endpoint<ProcessInvoiceRequest,
     Results<
         Ok<ProcessInvoiceResponse>,
         BadRequest,
@@ -12,10 +14,30 @@ public class Process : Endpoint<ProcessInvoiceRequest,
     public override void Configure()
     {
         Post("/invoice/process");
+        AllowAnonymous();
     }
 
     public override async Task HandleAsync(ProcessInvoiceRequest req, CancellationToken ct)
     {
-        throw new NotImplementedException();
+        var command = new ProcessInvoiceCommand
+        {
+            FileName = req.FileName,
+            ContentType = req.ContentType,
+            Base64Content = req.Base64Content
+        };
+
+        var result = await mediator.Send(command, ct);
+
+        if (result.IsSuccess)
+        {
+            await Send.ResultAsync(TypedResults.Ok<ProcessInvoiceResponse>(new()
+            {
+                IsSuccess = true
+            }));
+        }
+        else
+        {
+            await Send.ResultAsync(TypedResults.BadRequest());
+        }
     }
 }
