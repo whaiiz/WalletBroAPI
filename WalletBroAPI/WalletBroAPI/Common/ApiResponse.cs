ae7184621d482416137515ef2aa56e88c61c9a3d
Jaime Reis <jaime_reis18@outlook.pt>
2025-11-18 06:53:14 -0100

    Created generic ApiResponse.cs

---
+++ WalletBroAPI/WalletBroAPI/Common/ApiResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace WalletBroAPI.Common;
+
+public record ApiResponse<T>(
+    bool IsSuccess,
+    string Message,
+    T? Data = default,
+    IEnumerable<ErrorDetail>? Errors = null
+)
+{
+    public static ApiResponse<T> Success(string message, T data) =>
+        new(true, message, data, null);
+
+    public static ApiResponse<T> Error(string message, IEnumerable<ErrorDetail>? errors = null) =>
+        new(false, message, default, errors);
+}
+
+public record ErrorDetail(string Field, string Message);
