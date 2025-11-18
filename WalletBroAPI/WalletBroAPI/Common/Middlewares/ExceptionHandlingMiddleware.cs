128e3c99855853ec2c5694c44c053645d8c771bb
Jaime Reis <jaime_reis18@outlook.pt>
2025-11-18 06:52:16 -0100

    Created middleware for generic exceptions

---
+++ WalletBroAPI/WalletBroAPI/Common/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,28 @@
+﻿namespace WalletBroAPI.Common.Middlewares;
+
+public class ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Unhandled exception");
+
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            context.Response.ContentType = "application/json";
+
+            var response = new ApiResponse<object>(
+                IsSuccess: false,
+                Message: "An unexpected error occurred",
+                Data: null,
+                Errors: null
+            );
+
+            await context.Response.WriteAsJsonAsync(response);
+        }
+    }
+}
--- WalletBroAPI/WalletBroAPI/Program.cs
+++ WalletBroAPI/WalletBroAPI/Program.cs
@@ -42,6 +42,8 @@ if (app.Environment.IsDevelopment())
 }
 app.UseHttpsRedirection();
 
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+
 // app.UseAuthentication();  
 // app.UseAuthorization();
 
