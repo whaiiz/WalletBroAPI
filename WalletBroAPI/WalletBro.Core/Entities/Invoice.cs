f40815bdcf1bbd6dfaa6d397adcd670902003553
Jaime Reis <jaime_reis18@outlook.pt>
2025-11-18 06:56:06 -0100

    Improvements on core models: User and Invoice

--- WalletBroAPI/WalletBro.Core/Entities/Invoice.cs
+++ WalletBroAPI/WalletBro.Core/Entities/Invoice.cs
@@ -4,7 +4,7 @@ public class Invoice
 {
     public int Id { get; set; }
 
-    public int UserId { get; set; }
+    public string UserId { get; set; }
 
     public User? User { get; set; }
     
--- WalletBroAPI/WalletBro.Core/Entities/User.cs
+++ WalletBroAPI/WalletBro.Core/Entities/User.cs
@@ -2,15 +2,17 @@
 
 public class User
 {
-    public int Id { get; set; }
+    public required Guid Id { get; set; }
     
-    public string FirstName { get; set; } = string.Empty;
+    public required string FirstName { get; set; }
     
     public string LastName { get; set; } = string.Empty;
     
-    public string Email { get; set; } = string.Empty;
+    public required string Email { get; set; }
     
-    public DateTime DateOfBirth { get; set; } = DateTime.MinValue!;
+    public required string PasswordHash { get; set; }
+    
+    public required DateTime DateOfBirth { get; set; }
     
     public DateTime CreatedAt { get; set; } = DateTime.Now;
 }
