1b9334c47c69d02966ae439e93fd12c178d25126
Jaime Reis <jaime_reis18@outlook.pt>
2025-11-20 06:56:47 -0100

    Maspter configuration

---
+++ WalletBroAPI/WalletBroAPI/Common/Mapping/WebApiMaspterConfig.cs
@@ -0,0 +1,15 @@
+﻿using Mapster;
+using WalletBro.UseCases.User.Login;
+using WalletBro.UseCases.User.Register;
+using WalletBroAPI.User;
+
+namespace WalletBroAPI.Common.Mapping;
+
+public class WebApiMaspterConfig : IRegister 
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        config.NewConfig<RegisterRequest, RegisterCommand>();
+        config.NewConfig<LoginRequest, LoginCommand>();
+    }
+}
