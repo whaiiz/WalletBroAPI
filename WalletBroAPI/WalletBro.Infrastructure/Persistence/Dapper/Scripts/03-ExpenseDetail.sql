ece3dc31fe45f5c54fcb2b8f609970f2426dc127
Jaime Reis <jaime_reis18@outlook.pt>
2025-11-22 21:33:33 -0100

    Scripts updates

--- WalletBroAPI/WalletBro.Infrastructure/Persistence/Dapper/Scripts/03-ExpenseDetail.sql
+++ WalletBroAPI/WalletBro.Infrastructure/Persistence/Dapper/Scripts/03-ExpenseDetail.sql
@@ -1,9 +1,9 @@
 ﻿CREATE TABLE ExpenseDetail(
-    Id INT IDENTITY(1,1) PRIMARY KEY,
+    Id UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
     Name NVARCHAR(100),
     UnitPrice MONEY,
     UnitType NVARCHAR(50),
-    InvoiceId int not null,
+    InvoiceId UNIQUEIDENTIFIER not null,
     CreatedAt DATETIME2 DEFAULT GETDATE(),
     CONSTRAINT Fk_ExpenseDetail_Invoice
         FOREIGN KEY (InvoiceId)
