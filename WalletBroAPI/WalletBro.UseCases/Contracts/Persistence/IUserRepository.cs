6f016c545a7134bec23dba0bcb4cd22756bbe004
Jaime Reis <jaime_reis18@outlook.pt>
2025-11-24 18:13:58 -0100

    Create GetById on UserRepository.cs

--- WalletBroAPI/WalletBro.UseCases/Contracts/Persistence/IUserRepository.cs
+++ WalletBroAPI/WalletBro.UseCases/Contracts/Persistence/IUserRepository.cs
@@ -1,10 +1,10 @@
-﻿using WalletBro.Core.Entities;
-
-namespace WalletBro.UseCases.Contracts.Persistence;
+﻿namespace WalletBro.UseCases.Contracts.Persistence;
 
 public interface IUserRepository
 {
     Task<Core.Entities.User?> GetByEmailAsync(string email);
+    
+    Task<Core.Entities.User?> GetById(string id);
     
     Task<bool> AddAsync(Core.Entities.User user);
 }
