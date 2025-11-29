73ef0e60d5b658aab6ed4f9d8da1e1d3d6b6235d
Jaime Reis <jaime_reis18@outlook.pt>
2025-11-29 11:02:28 -0100

    GetInvoices endpoint

---
+++ WalletBroAPI/WalletBro.UseCases/Invoice/GetInvoices/GetInvoicesHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using WalletBro.UseCases.Contracts.Authentication;
+using WalletBro.UseCases.Contracts.Persistence;
+
+namespace WalletBro.UseCases.Invoice.GetInvoices;
+
+public class GetInvoicesHandler(ICurrentUserService currentUserService, IInvoiceRepository invoiceRepository) : 
+    IRequestHandler<GetInvoicesQuery, GetInvoicesResult>
+{
+    public async Task<GetInvoicesResult> Handle(GetInvoicesQuery request, CancellationToken cancellationToken)
+    {
+        var result = new GetInvoicesResult();
+        var userId = currentUserService.UserId;
+
+        if (userId == Guid.Empty) return result;
+
+        var invoices = await invoiceRepository.GetAllAsync(userId);
+
+        result.Invoices = invoices.ToList();
+        return result;
+    }
+}
---
+++ WalletBroAPI/WalletBroAPI/Invoice/GetInvoices.cs
@@ -0,0 +1,33 @@
+﻿using FastEndpoints;
+using Mapster;
+using MediatR;
+using WalletBro.UseCases.Invoice.GetInvoices;
+using WalletBroAPI.Common;
+using WalletBroAPI.Dtos.Invoice;
+
+namespace WalletBroAPI.Invoice;
+
+public class GetInvoices(IMediator mediator) : EndpointWithoutRequest<ApiResponse<GetInvoicesResponse>>
+{
+    public override void Configure()
+    {
+        Get("/invoices");
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        var command = new GetInvoicesQuery();
+        var invoice = await mediator.Send(command, ct);
+        var response = new GetInvoicesResponse()
+        {
+            Invoices =  invoice.Invoices.Adapt<List<InvoiceDto>>()
+        };
+        
+        var successResponse = ApiResponse<GetInvoicesResponse>.Success(
+            message: string.Empty,
+            data: response
+        );
+            
+        await Send.OkAsync(successResponse, cancellation: ct);
+    }
+}
